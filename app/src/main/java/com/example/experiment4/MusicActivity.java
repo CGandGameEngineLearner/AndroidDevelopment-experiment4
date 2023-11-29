b2a61cdc3f2cfeb0ba099178ee32c53764849530
CGandGameEngineLearner <1353321741@qq.com>
2023-11-29 21:25:01 +0800

    develop:music player complete

--- app/src/main/java/com/example/experiment4/MusicActivity.java
+++ app/src/main/java/com/example/experiment4/MusicActivity.java
@@ -3,6 +3,7 @@ package com.example.experiment4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,7 +13,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class MusicBean{
     public String mMusicName;
@@ -45,13 +49,15 @@ public class MusicActivity extends AppCompatActivity {
     {
         mCurrentMusicIndex=MusicIndex;
         MusicBean musicBean=mMusicBeans.get(MusicIndex);
+        mMediaPlayer.stop();
+        mMediaPlayer.release();
         mMediaPlayer=MediaPlayer.create(this, musicBean.mMusicResID);
         mMediaPlayer.start();
     }
 
     public void playNextMusic()
     {
-        playMusic(mCurrentMusicIndex%mMusicBeans.size());
+        playMusic((mCurrentMusicIndex+1)%mMusicBeans.size());
     }
 
     public void stopMusic()
@@ -97,8 +103,8 @@ public class MusicActivity extends AppCompatActivity {
             }
         });
     }
-    private ArrayList<String> mMusicNames;
-    private ArrayList<MusicBean> mMusicBeans;
+    private ArrayList<String> mMusicNames=new ArrayList<String>();
+    private ArrayList<MusicBean> mMusicBeans=new ArrayList<MusicBean>();
     private void loadListView()
     {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.music_list_item, mMusicNames);
@@ -107,7 +113,7 @@ public class MusicActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent IntentDetail=new Intent(MusicActivity.this,MusicDetailActivity.class);
-                MusicBean musicBean=mMusicBeans.get(position-1);
+                MusicBean musicBean=mMusicBeans.get(position);
                 IntentDetail.putExtra("MusicName",musicBean.mMusicName);
                 IntentDetail.putExtra("MusicResID",musicBean.mMusicResID);
                 IntentDetail.putExtra("SingerName",musicBean.mSingerName);
@@ -117,11 +123,28 @@ public class MusicActivity extends AppCompatActivity {
         });
     }
 
+    private void loadMusicAsset()
+    {
+        MusicBean[] musicBeans={
+                new MusicBean("moos",R.raw.moss,"歌手","歌词"),
+                new MusicBean("说好不哭",R.raw.shuo_hao_bu_ku,"周杰伦","歌词")
+        };
+        for(MusicBean musicBean:musicBeans)
+        {
+            Log.d("Music",musicBean.mMusicName);
+            mMusicNames.add(musicBean.mMusicName);
+            mMusicBeans.add(musicBean);
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
         initButtons();
         mListView=findViewById(R.id.listView);
+        loadMusicAsset();
+        loadListView();
     }
 }
--- app/src/main/java/com/example/experiment4/MusicDetailActivity.java
+++ app/src/main/java/com/example/experiment4/MusicDetailActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.experiment4.MusicBean;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MusicDetailActivity extends AppCompatActivity {
 
@@ -19,10 +20,24 @@ public class MusicDetailActivity extends AppCompatActivity {
 
     private MusicBean mMusicBean;
 
+    private TextView textViewMusicName,textViewSingerName,textViewLyric;
+
+    private void init()
+    {
+        textViewMusicName=findViewById(R.id.textViewMusicName);
+        textViewSingerName=findViewById(R.id.textViewSingerName);
+        textViewLyric=findViewById(R.id.textViewLyric);
+
+        textViewMusicName.setText(mMusicBean.mMusicName);
+        textViewSingerName.setText(mMusicBean.mSingerName);
+        textViewLyric.setText(mMusicBean.mLyric);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_detail);
         mMusicBean=getMusicBean();
+        init();
     }
 }
